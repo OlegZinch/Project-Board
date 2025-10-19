6194a67934815d5718559d0c4235bf77a1009eff
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-10-19 21:23:57 +0100

    Working with 'preventDefault()'

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -19,17 +19,24 @@ buttons.forEach((btn) => {
   btn.addEventListener('mouseenter', buttonClickHandler)
 })
 
-let curElementNumber = 0
+// let curElementNumber = 0
 
-function scrollHandler() {
-  const distanceToBottom = document.body.getBoundingClientRect().bottom
+// function scrollHandler() {
+//   const distanceToBottom = document.body.getBoundingClientRect().bottom
 
-  if (distanceToBottom < document.documentElement.clientHeight + 150) {
-    const newDataElement = document.createElement('div')
-    curElementNumber++
-    newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`
-    document.body.append(newDataElement)
-  }
-}
+//   if (distanceToBottom < document.documentElement.clientHeight + 150) {
+//     const newDataElement = document.createElement('div')
+//     curElementNumber++
+//     newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`
+//     document.body.append(newDataElement)
+//   }
+// }
 
-window.addEventListener('scroll', scrollHandler)
+// window.addEventListener('scroll', scrollHandler)
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log(event)
+})
