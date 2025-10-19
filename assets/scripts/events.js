94827d3aa98cc8e85e994bacbf17003742987c0e
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-10-19 21:11:22 +0100

    Example: Basic Infinite Scrolling

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -19,6 +19,17 @@ buttons.forEach((btn) => {
   btn.addEventListener('mouseenter', buttonClickHandler)
 })
 
-window.addEventListener('scroll', (event) => {
-  console.log(event)
-})
+let curElementNumber = 0
+
+function scrollHandler() {
+  const distanceToBottom = document.body.getBoundingClientRect().bottom
+
+  if (distanceToBottom < document.documentElement.clientHeight + 150) {
+    const newDataElement = document.createElement('div')
+    curElementNumber++
+    newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`
+    document.body.append(newDataElement)
+  }
+}
+
+window.addEventListener('scroll', scrollHandler)
